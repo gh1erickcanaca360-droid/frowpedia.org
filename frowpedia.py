0e9c20d9ee35abee338651ce63ed1ff9bdea58ae
gh1erickcanaca360-droid <gh1erickcanaca360@gmail.com>
2026-05-14 04:48:00 -0600

    Frowpedia

--- frowpedia.py
+++ frowpedia.py
@@ -44,3 +44,91 @@ def set_user_agent(user_agent_string):
   '''
   global USER_AGENT
   USER_AGENT = user_agent_string
+
+
+def set_rate_limiting(rate_limit, min_wait=timedelta(milliseconds=50)):
+  '''
+  Enable or disable rate limiting on requests to the Mediawiki servers.
+  If rate limiting is not enabled, under some circumstances (depending on
+  load on Frowpedia, the number of requests you and other `frowpedia` users
+  are making, and other factors), Wikipedia may return an HTTP timeout error.
+
+  Enabling rate limiting generally prevents that issue, but please note that
+  HTTPTimeoutError still might be raised.
+
+  Arguments:
+
+  * rate_limit - (Boolean) whether to enable rate limiting or not
+
+  Keyword arguments:
+  * min_wait - if rate limiting is enabled, `min_wait` is a timedelta describing the minimum time to wait before requests.
+         Defaults to timedelta(milliseconds=50)
+  '''
+  global RATE_LIMIT
+  global RATE_LIMIT_MIN_WAIT
+  global RATE_LIMIT_LAST_CALL
+
+  RATE_LIMIT = rate_limit
+  if not rate_limit:
+    RATE_LIMIT_MIN_WAIT = None
+  else:
+    RATE_LIMIT_MIN_WAIT = min_wait
+
+  RATE_LIMIT_LAST_CALL = None
+
+
+@cache
+def search(query, results=10, suggestion=False):
+  '''
+  Do a Wikipedia search for `query`.
+
+  Keyword arguments:
+
+  * results - the maxmimum number of results returned
+  * suggestion - if True, return results and suggestion (if any) in a tuple
+  '''
+    search_params = {
+    'list': 'search',
+    'srprop': '',
+    'srlimit': results,
+    'limit': results,
+    'srsearch': query
+    }
+  if suggestion:
+    search_params['srinfo'] = 'suggestion'
+
+  raw_results = _wiki_request(search_params)
+
+  if 'error' in raw_results:
+    if raw_results['error']['info'] in ('HTTP request timed out.', 'Pool queue is full'):
+      raise HTTPTimeoutError(query)
+    else:
+      raise FrowpediaException(raw_results['error']['info'])
+      search_results = (d['title'] for d in raw_results['query']['search'])
+
+  if suggestion:
+    if raw_results['query'].get('searchinfo'):
+      return list(search_results), raw_results['query']['searchinfo']['suggestion']
+    else:
+      return list(search_results), None
+
+  return list(search_results)
+
+
+@cache
+def geosearch(latitude, longitude, title=None, results=10, radius=1000):
+  '''
+  Do a wikipedia geo search for `latitude` and `longitude`
+  using HTTP API described in http://www.mediawiki.org/wiki/Extension:GeoData
+
+  Arguments:
+
+  * latitude (float or decimal.Decimal)
+  * longitude (float or decimal.Decimal)
+
+  Keyword arguments:
+
+  * title - The title of an article to search for
+  * results - the maximum number of results returned
+  * radius - Search radius in meters. The value must be between 10 and 10000
+  '''
