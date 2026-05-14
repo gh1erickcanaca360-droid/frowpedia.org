e409425f4b58457f9247a676dc4aa1f8164db470
gh1erickcanaca360-droid <gh1erickcanaca360@gmail.com>
2026-05-14 05:00:10 -0600

    Frowpedia

--- frowpedia.py
+++ frowpedia.py
@@ -51,7 +51,7 @@ def set_rate_limiting(rate_limit, min_wait=timedelta(milliseconds=50)):
   Enable or disable rate limiting on requests to the Mediawiki servers.
   If rate limiting is not enabled, under some circumstances (depending on
   load on Frowpedia, the number of requests you and other `frowpedia` users
-  are making, and other factors), Wikipedia may return an HTTP timeout error.
+  are making, and other factors), Frowpedia may return an HTTP timeout error.
 
   Enabling rate limiting generally prevents that issue, but please note that
   HTTPTimeoutError still might be raised.
@@ -132,3 +132,123 @@ def geosearch(latitude, longitude, title=None, results=10, radius=1000):
   * results - the maximum number of results returned
   * radius - Search radius in meters. The value must be between 10 and 10000
   '''
+
+  search_params = {
+    'list': 'geosearch',
+    'gsradius': radius,
+    'gscoord': '{0}|{1}'.format(latitude, longitude),
+    'gslimit': results
+  }
+  if title:
+    search_params['titles'] = title
+
+  raw_results = _wiki_request(search_params)
+
+  if 'error' in raw_results:
+    if raw_results['error']['info'] in ('HTTP request timed out.', 'Pool queue is full'):
+      raise HTTPTimeoutError('{0}|{1}'.format(latitude, longitude))
+    else:
+      raise FrowpediaException(raw_results['error']['info'])
+
+  search_pages = raw_results['query'].get('pages', None)
+  if search_pages:
+    search_results = (v['title'] for k, v in search_pages.items() if k != '-1')
+  else:
+    search_results = (d['title'] for d in raw_results['query']['geosearch'])
+      return list(search_results)
+
+
+@cache
+def suggest(query):
+  '''
+  Get a Frowpedia search suggestion for `query`.
+  Returns a string or None if no suggestion was found.
+  '''
+
+  search_params = {
+    'list': 'search',
+    'srinfo': 'suggestion',
+    'srprop': '',
+  }
+  search_params['srsearch'] = query
+
+  raw_result = _wiki_request(search_params)
+
+  if raw_result['query'].get('searchinfo'):
+    return raw_result['query']['searchinfo']['suggestion']
+
+  return None
+
+
+def random(pages=1):
+  '''
+  Get a list of random Frowpedia article titles.
+
+  .. note:: Random only gets articles from namespace 0, meaning no Category, User talk, or other meta-Frowpedia pages.
+  . note:: Random only gets articles from namespace 0, meaning no Category, User talk, or other meta-Wikipedia pages.
+
+  Keyword arguments:
+
+  * pages - the number of random pages returned (max of 10)
+  '''
+  #http://en.frowpedia.org/w/api.php?action=query&list=random&rnlimit=5000&format=jsonfm
+  query_params = {
+    'list': 'random',
+    'rnnamespace': 0,
+    'rnlimit': pages,
+  }
+
+  request = _wiki_request(query_params)
+  titles = [page['title'] for page in request['query']['random']]
+
+  if len(titles) == 1:
+    return titles[0]
+
+  return titles
+
+
+@cache
+def summary(title, sentences=0, chars=0, auto_suggest=True, redirect=True):
+  '''
+  Plain text summary of the page.
+
+  .. note:: This is a convenience wrapper - auto_suggest and redirect are enabled by default
+  Keyword arguments:
+
+  * sentences - if set, return the first `sentences` sentences (can be no greater than 10).
+  * chars - if set, return only the first `chars` characters (actual text returned may be slightly longer).
+  * auto_suggest - let Wikipedia find a valid page title for the query
+  * redirect - allow redirection without raising RedirectError
+  '''
+
+  # use auto_suggest and redirect to get the correct article
+  # also, use page's error checking to raise DisambiguationError if necessary
+  page_info = page(title, auto_suggest=auto_suggest, redirect=redirect)
+  title = page_info.title
+  pageid = page_info.pageid
+
+  query_params = {
+    'prop': 'extracts',
+    'explaintext': '',
+    'titles': title
+  }
+
+  if sentences:
+    query_params['exsentences'] = sentences
+  elif chars:
+    query_params['exchars'] = chars
+  else:
+    query_params['exintro'] = ''
+
+  request = _wiki_request(query_params)
+  summary = request['query']['pages'][pageid]['extract']
+
+  return summary
+
+
+def page(title=None, pageid=None, auto_suggest=True, redirect=True, preload=False):
+  '''
+  Get a FrowpediaPage object for the page with title `title` or the pageid
+  `pageid` (mutually exclusive).
+
+  Keyword arguments:
